ddf150102ccda14727d805ba8d33f59c71e9dad2
unknown <rohith536shetty@gmail.com>
2023-12-01 15:47:27 +0530

    Remove unnecessary line of code

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,3 @@
-//console.log("The script is running successfully in the terminal");
-
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data);
-    })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
